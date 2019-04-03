2d8c2f7e007bc81e1c7b970c569bb0513bc43918
Nick <nickfarrenkopf@gmail.com>
2019-04-02 21:32:04 -0700

    pacman vae training

--- Emulator/trainer.py
+++ Emulator/trainer.py
@@ -35,17 +35,17 @@ if __name__ == '__main__':
 
     # class data
     class_path = paths.network_path
-    size = 128000
+    size = 64
     n_classes = 4
 
 
 
     """ DATA """
 
-    if 1: # AUTO DATA
+    if 0: # AUTO DATA
         ds = FT.load_images(files[:n])
         ds = DT.subdata(ds, h, w)
-        print('Data shape: {}'.format(ds.shape))
+        print('Data shape: {} {} {}'.format(ds.shape, ds.max(), ds.min()))
 
     if 0: # CLASS DATA
         labels = set(('0','1','2'))
@@ -60,41 +60,36 @@ if __name__ == '__main__':
 
     if 1: # CREATE
         #auto_hidden = [32, 32, 32, 16, 8, 4]
-        auto_hidden = [32, 32, 16, 16, 8, 4]
-        #auto_hidden = [128, 64, 32]
-        AUTO.new(paths, name, h, w, auto_hidden, length=le, patch=3, e=1e-8,
-                 reuse_weights=True, print_me=True, ps=2)
+        paths.reset_json()
+        hidde_encode = [2, 3, 4, 5, 6, 7]
+        pools_encode = [2, 2, 2, 2, 2, 2]
+        hidde_decode = [6, 5, 4, 3, 2, 1]
+        pools_decode = [1, 1, 1, 1, 1, 1]
+        hidden_dense = [512, 512]
+        n_latent = 64
+        hidden_encode = [2 ** i for i in hidde_encode]
+        hidden_decode = [2 ** i for i in hidde_decode]
+        AUTO.new(paths, name, h, w, length=le, patch=5, print_me=True,
+                 hidden_encode=hidden_encode, pools_encode=pools_encode,
+                 hidden_latent=hidden_dense, n_latent=n_latent,
+                 pools_decode=pools_decode, hidden_decode=hidden_decode)
 
     if 1: # LOAD - NETWORK
         auto_network = AUTO.load(name, paths.load_json())
         #auto_network.print_info()
 
 
-    if 1: # TRAIN ITER - DATA
+    if 0: # TRAIN ITER - DATA
         print('Training on data with iters')
-        AUTO.train_data_iter(auto_network, ds, h, w, n_train=100, a=1e-3,
-                             n_plot=4, plot_r=True, plot_i=True,
-                             do_subdata=False, kmax_img=5, kmax_cost=2)
-    
-    if 0: # TRAIN FULL - DATA
-        print('Training on data until finished')
-        start = time.time()
-        AUTO.train_data_full(auto_network, ds, h, w, alpha=1e-3, n_plot=n//2,
-                             do_subdata=True, kmax_cost=20)
-        print('Time {}'.format(time.time() - start))
-    
+        AUTO.train_data_iter(auto_network, ds, h, w, n_train=10000, a=1e-3,
+                             n_plot=20, plot_r=True, plot_i=True,
+                             do_subdata=False, kmax_img=2000, kmax_cost=50)
 
-    if 0: # TRAIN ITER - PATHS
+    if 1: # TRAIN ITER - PATHS
         print('Training on data with iters')
-        AUTO.train_path_iter(auto_network, files, h, w, h_f, w_f, n_train=100,
-                             alpha=1e-3, n_plot=n//2, plot_r=True, plot_i=True,
-                             kmax_cost=10, kmax_img=20, do_subdata=True)
-
-    if 0: # TRAIN FULL - PATHS
-        print('Training on data with iters')
-        AUTO.train_path_full(auto_network, files, h, w, h_f, w_f, alpha=1e-4, 
-                             n_plot=n//2, do_subdata=True, kmax_cost=20)
-
+        AUTO.train_path_iter(auto_network, files, h, w, n_train=100000,
+                             a=1e-3, n_plot=16, plot_r=True, plot_i=True,
+                             kmax_cost=500, kmax_img=2000, do_subdata=True)
 
 
     """ EMBEDDING """ 
